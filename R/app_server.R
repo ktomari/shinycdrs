284758c075cac7624836edf8ecb78fbc94205e37
ktomari <ktomari@ucdavis.edu>
2024-07-06 18:55:35 -0700

    temporary removed crosstabs

--- R/app_server.R
+++ R/app_server.R
@@ -5,8 +5,9 @@
 #' @import shiny
 #' @noRd
 app_server <- function(input, output, session) {
-
-  # Your application server logic
-  mod_crosstabs_server("crosstabs_1")
+  
+  # Call module server functions
+  # mod_crosstabs_server("crosstabs_1")
   mod_graphs_server("graphs_1")
 }
+
