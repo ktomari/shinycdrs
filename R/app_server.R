90d5ac356ca00f19cd3c05e4a960c54a7e20d753
ktomari <ktomari@ucdavis.edu>
2024-07-28 15:37:31 -0700

    Added html home page

--- R/app_server.R
+++ R/app_server.R
@@ -5,6 +5,14 @@
 #' @import shiny
 #' @noRd
 app_server <- function(input, output, session) {
+  # # Read the HTML file content
+  # htmlFile <- file.path("inst", "app", "www", "homepage.html")
+  # htmlContent <- readLines(htmlFile, warn = FALSE)
+  # 
+  # # Render the HTML content
+  # output$home_html <- renderUI({
+  #   HTML(paste(htmlContent, collapse = "\n"))
+  # })
   
   # Call module server functions
   # mod_crosstabs_server("crosstabs_1")
