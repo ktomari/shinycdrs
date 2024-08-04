f2b204ea394123ee04da73c5a4aba1c60bd75437
ktomari <ktomari@ucdavis.edu>
2024-08-04 14:03:04 -0700

    Cleaned and added crosstabs

--- R/app_server.R
+++ R/app_server.R
@@ -5,17 +5,8 @@
 #' @import shiny
 #' @noRd
 app_server <- function(input, output, session) {
-  # # Read the HTML file content
-  # htmlFile <- file.path("inst", "app", "www", "homepage.html")
-  # htmlContent <- readLines(htmlFile, warn = FALSE)
-  # 
-  # # Render the HTML content
-  # output$home_html <- renderUI({
-  #   HTML(paste(htmlContent, collapse = "\n"))
-  # })
-  
   # Call module server functions
-  # mod_crosstabs_server("crosstabs_1")
+  mod_crosstabs_server("crosstabs_1")
   mod_graphs_server("graphs_1")
 }
 
