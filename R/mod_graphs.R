5b7d93f99832fe3a3c5856a098584c09f2511503
ktomari <ktomari@ucdavis.edu>
2024-08-04 14:05:33 -0700

    Fixed text labels, added plot options for sizing of image, turned weighting off for demographics.

--- R/mod_graphs.R
+++ R/mod_graphs.R
@@ -1,4 +1,4 @@
-#' graphs UI Function
+#' Graphs UI Function
 #'
 #' @description A shiny Module.
 #'
@@ -11,43 +11,62 @@ mod_graphs_ui <- function(id){
   ns <- shiny::NS(id)
  
   shiny::tagList(
-    shinyMobile::f7Accordion(
-      id = "graph_accordion",
-      side = "left",
-      shinyMobile::f7AccordionItem(
-        title = "Survey Section Selection",
-        open = TRUE,
-        shinyMobile::f7Select(
-          inputId = shiny::NS(id, "selected_sections"),
-          label = NULL,
-          choices = unique(env_dat$params_graphs$block),
-          style = list(
-            description = NULL,
-            media = shinyMobile::f7Icon("menu"),
-            outline = FALSE
+    shinyMobile::f7Card(
+      shinyMobile::f7Select(
+        label = "Survey Section",
+        inputId = shiny::NS(id, "selected_sections"),
+        choices = unique(env_dat$params_graphs$block),
+        style = list(
+                description = NULL,
+                media = shinyMobile::f7Icon("menu"),
+                outline = FALSE
+              )
+      ),
+      footer = shinyMobile::f7Accordion(
+        id = "graph_accordion",
+        side = "left",
+        shinyMobile::f7AccordionItem(
+          title = "Plot Options",
+          open = FALSE,
+          shinyMobile::f7List(
+            shiny::div(
+              class = "padded-input",
+              shinyMobile::f7Stepper(
+                inputId = shiny::NS(id, "font_size"),
+                label = "Font Size",
+                min = 10,
+                max = 100,
+                step = 5,
+                size = "small",
+                value = 20,
+                wraps = TRUE,
+                autorepeat = TRUE,
+                rounded = FALSE,
+                raised = FALSE,
+                manual = FALSE
+              )
+            ),
+            shiny::div(
+              class = "padded-input",
+              shinyMobile::f7Stepper(
+                inputId = shiny::NS(id, "plotHeight"),
+                label = "Plot Height (px)",
+                min = 400,
+                max = 1000,
+                step = 75,
+                size = "small",
+                value = 475,
+                wraps = FALSE,
+                autorepeat = TRUE,
+                rounded = FALSE,
+                raised = FALSE,
+                manual = FALSE
+              )
+            )
           )
         )
-      ),
-      # TODO adjust, improve, and polish the Advanced Options
-      shinyMobile::f7AccordionItem(
-        title = "Advanced Options",
-        open = FALSE
-        # shiny::sliderInput(
-        #   inputId = shiny::NS(id, "height"),
-        #   label = "height",
-        #   min = 100,
-        #   max = 500,
-        #   value = 250
-        # ), 
-        # shiny::sliderInput(
-        #   inputId = shiny::NS(id, "width"),
-        #   label = "width",
-        #   min = 100,
-        #   max = 500,
-        #   value = 250
-        # )
-      )
-    ),  # END accordion
+      ),  # END accordion
+    ),
     shiny::uiOutput(ns("graph_cards"))
   )  # END taglist
 }
@@ -64,7 +83,7 @@ mod_graphs_server <- function(id){
     # returns list of necessary specifications for cdrs::plt...
     rct_preps <- shiny::reactive({
       if(length(input$selected_sections) == 1){
-        # Using "graphs" tab in "shiny_paramters.xlsx",
+        # Using "graphs" tab in "shiny_parameters.xlsx",
         # filter this section (from the summary report),
         # then, by question, split table into list of tables
         vars_ <- env_dat$params_graphs %>%
@@ -89,11 +108,33 @@ mod_graphs_server <- function(id){
         # create card id
         card_id <- ns(paste("plot", i, sep = "_"))
         
-        title_ <- rct_preps()[[i]]$title
+        prep_ <- rct_preps()[[i]]
         
+        caps_ <- prep_$captions
+        
+        title_ <- prep_$title
+        
+        # f7Card ----
         shinyMobile::f7Card(
+          # Title.
           title = title_,
-          shiny::plotOutput(outputId = card_id)
+          # Subtitle (ie. survey question and pLabel).
+          # if `pLabel` exists, display it as a HTML paragraph
+          if(!is.na(prep_$pLabel[1])){
+            graph_label(prep_$id, prep_$pLabel)
+            },
+          footer = shinyMobile::f7Accordion(
+            side = "left",
+            shinyMobile::f7AccordionItem(
+              title = "Details",
+              shinyMobile::f7Block(paste0(caps_, collapse = " "))
+            )
+          ),
+          # footer = ,
+          shiny::plotOutput(outputId = card_id),
+          # TODO
+          # This is just a clumsy workaround for the issue of changing plot image size which is an Advanced Option. The core problem is that if you enlarge the ggplot2 rendered image, it doesn't also adjust the height of the f7Card. This workaround anticipates the height of the card to be 10% taller. The issue is that with increasing heights, more whitespace is added below hte ggplot2 image. This is not the most elegant solution.
+          height = input$plotHeight * 1.1
         )
       })
       
@@ -110,8 +151,12 @@ mod_graphs_server <- function(id){
         # extract the plt prep object for this iteration.
         prep_ <- rct_preps()[[i]]
         # remove the caption
-        prep_$caption <- NULL
+
+        prep_$captions <- NULL
+        
         prep_$title <- NULL
+        
+        prep_$title_size <- input$font_size
         
         # Generate the plot based on type
         plt_ <- switch(prep_$type,
@@ -122,6 +167,9 @@ mod_graphs_server <- function(id){
                        NULL  # Default case if none match
         )
         
+        height_ <- input$plotHeight
+        
+        # renderPlot ----
         # render the image and save it into output.
         if (!inherits(plt_, "NULL")) {
           output[[card_id]] <- 
@@ -129,7 +177,8 @@ mod_graphs_server <- function(id){
             # custom size adjustment.
             # width = function() input$width,
             # height = function() input$height,
-            {plt_})
+            {plt_}, 
+            height = reactive({ height_ }))
         } else {
           message("Plot object is NULL for index: ", i)
         }
--- R/mod_graphs_fct.R
+++ R/mod_graphs_fct.R
@@ -4,16 +4,33 @@
 #' 
 #' @return list
 mod_graph_prep <- function(l){
+
   purrr::map(.x = l, .f = function(tb){
     # First obtain a subset of the data dictionary
     # for the question in tb, eg. Q1_*
-    qids_ <- tb$Variable
+    qids_ <- tb$Variable %>%
+      unique()
     
+    # retrieve params_labels for this qid.
+    plabs_ <- env_dat$params_labels %>%
+      dplyr::filter(Variable %in% qids_)
+    
+    # Determine weighting.
+    params_g <- env_dat$params_graphs %>%
+      dplyr::filter(Variable %in% qids_)
+    
+    if("Demographics" %in% params_g$block){
+      set_wgt <- FALSE
+    } else {
+      set_wgt <- TRUE
+    }
+    
+    # get prep object
     prep_ <- cdrs::cdrs_plt_prep(
       data_ = env_dat$dat$data,
       cols_ = qids_,
       dict_ = env_dat$dat$dict,
-      is_weighted = TRUE,  # TODO make conditional
+      is_weighted = set_wgt,
       txt_options = list(
         label_form = "short",
         title_form = "short",
@@ -21,8 +38,43 @@ mod_graph_prep <- function(l){
         caption_ = T
       )
     )
+
+    if(nrow(plabs_) > 0){
+      prep_$id <- plabs_$id %>%
+        unique()
+      
+      prep_$pLabel <- plabs_$Label %>% 
+        unique()
+    } else {
+      prep_$pLabel <- NA
+    }
     
     # return
     prep_
   })
+}
+
+#' @title Collapse id and text label
+#' 
+#' @description
+#' Using `paste0`, combine the id and lab, eg. "1. Do you..." and possibly combining multiple labels together, eg. "19a. Do you... & 19b. Do you..."
+#' 
+#' @param id character. Text-based id from the CDRS.
+#' @param lab character. Question from CDRS.
+#' @return shiny tag p object.
+#' @noRd
+graph_label <- function(id, lab){
+  full_questions <- purrr::map2_chr(.x = id, 
+                                    .y = lab, 
+                                    .f = ~paste0(c(.x, .y), collapse = ". "))
+  if(length(full_questions) > 1){
+    full_questions <- lapply(full_questions, function(x) {
+      shiny::tagList(x, shiny::tags$br())
+    })
+  } else {
+    full_questions <- shiny::tagList(full_questions)
+  }
+  
+  # return
+  do.call(shiny::tags$p, full_questions)
 }
