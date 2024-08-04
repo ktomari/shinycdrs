c11e892c81a496148ec590136f9678db52cd7cbb
ktomari <ktomari@ucdavis.edu>
2024-08-04 14:03:53 -0700

    Refined css, added crosstabs, fixed resource issue (ie. access to /www).

--- R/app_ui.R
+++ R/app_ui.R
@@ -1,48 +1,3 @@
-#' The application User-Interface
-#'
-#' @param request Internal parameter for `{shiny}`.
-#'     DO NOT REMOVE.
-#' @import shiny
-#' @noRd
-app_ui <- function(request) {
-  # begin shinyMobile page
-  shinyMobile::f7Page(
-    options = list(
-      dark = FALSE  # Set the default theme to light
-    ),  
-    # Layout
-    shinyMobile::f7TabLayout(
-      # Layout Navbar
-      navbar = shinyMobile::f7Navbar(
-        title = "California Delta Residents Survey Data Explorer",
-        hairline = TRUE,
-        leftPanel = FALSE,
-        rightPanel = FALSE
-      ),  # END navbar
-      # Tabs
-      shinyMobile::f7Tabs(
-        animated = TRUE,
-        shinyMobile::f7Tab(
-          title = "Home",
-          tabName = "home",
-          icon = shinyMobile::f7Icon("house"),
-          shinyMobile::f7Block(
-            # htmlOutput("home_html")
-            shiny::includeHTML("inst/app/www/homepage.html")
-          )
-        ),
-        shinyMobile::f7Tab(
-          title = "Graphs",
-          tabName = "graphs",
-          icon = shinyMobile::f7Icon("chart_bar_alt_fill"),
-          mod_graphs_ui("graphs_1")
-        )
-      )  # END tabs
-    )  # END tab layout
-  )  # END page
-  
-}
-
 #' Add external Resources to the Application
 #'
 #' This function is internally used to add external
@@ -56,14 +11,88 @@ golem_add_external_resources <- function() {
     "www",
     app_sys("app/www")
   )
-
+  
   tags$head(
     favicon(ext = "png"),
     bundle_resources(
       path = app_sys("app/www"),
       app_title = "shinycdrs"
-    )
+    ),
     # Add here other external resources
     # for example, you can add shinyalert::useShinyalert()
+    # Link to custom CSS file
+    tags$link(rel = "stylesheet", 
+              type = "text/css", 
+              href = "www/custom.css")
   )
 }
+
+#' The application User-Interface
+#'
+#' @param request Internal parameter for `{shiny}`.
+#'     DO NOT REMOVE.
+#' @import shiny
+#' @noRd
+app_ui <- function(request) {
+  shiny::tagList(
+    golem_add_external_resources(),
+    # begin shinyMobile page
+    shinyMobile::f7Page(
+      options = list(
+        dark = FALSE  # Set the default theme to light
+      ),  
+      # Layout
+      shinyMobile::f7TabLayout(
+        # Add custom CSS for padding
+        # tags$style(
+        #   
+        # ),
+        # Layout Navbar
+        navbar = shinyMobile::f7Navbar(
+          title = "California Delta Residents Survey Data Explorer",
+          hairline = TRUE,
+          leftPanel = FALSE,
+          rightPanel = FALSE
+        ),  # END navbar
+        # Tabs
+        shinyMobile::f7Tabs(
+          animated = TRUE,
+          # Tab 1: Home
+          shinyMobile::f7Tab(
+            title = "Home",
+            tabName = "home",
+            icon = shinyMobile::f7Icon("house"),
+            shinyMobile::f7Accordion(
+              id = "home_accordion",
+              side = "left",
+              # Content, derived from env_dat$home in zzz.R
+              purrr::map(.x = env_dat$home,
+                         .f = ~do.call(shinyMobile::f7AccordionItem,
+                                       .x)
+              )
+            )
+            
+          ),
+          # Tab 2: Graphs
+          shinyMobile::f7Tab(
+            title = "Graphs",
+            tabName = "graphs",
+            icon = shinyMobile::f7Icon("chart_bar_alt_fill"),
+            mod_graphs_ui("graphs_1")
+          ),
+          # Tab 3: Tables
+          shinyMobile::f7Tab(
+            title = "Cross Tables",
+            tabName = "tables",
+            icon = shinyMobile::f7Icon("table"),
+            mod_crosstabs_ui("crosstabs_1")
+          )
+        )  # END tabs
+      )  # END tab layout
+    )  # END page
+  )  # END tagList
+  
+  
+}
+
+
