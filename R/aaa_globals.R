323e3bc241852f293235d8507897c10b92710076
ktomari <ktomari@ucdavis.edu>
2024-08-08 09:56:15 -0700

    Added commented code for posterity (used in installing google fonts)

--- R/aaa_globals.R
+++ R/aaa_globals.R
@@ -9,5 +9,11 @@
 # global environment, reducing the risk of conflicts and making the data
 # management more modular and maintainable.
 
+# If you're looking for other startup scripts, see R/zzz.R
+
 # Initialize the custom environment for storing app data
 env_dat <- new.env(parent = emptyenv())
+
+# This code was used to install google fonts
+# gfonts::setup_font(id = "roboto-slab", output_dir = "inst/app/www")
+# gfonts::setup_font(id = "montserrat", output_dir = "inst/app/www")
